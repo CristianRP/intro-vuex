4d31111c07af3a1894b4e25a33680f268f0adc8b
Cristian Ramirez <cristianramirezgt@gmail.com>
2022-05-13 14:37:04 -0600

    20220513 143703

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CounterPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineAsyncComponent } from '@vue/runtime-core'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterPage: defineAsyncComponent(() => /* webpackChunkName="CounterPage" */ import('@/components/CounterPage'))
   }
 }
 </script>
